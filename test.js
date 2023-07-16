f3f0c47944f0f1a077ad9a24f0218289e185c55b
bhashitha <bhashitha@wealthos.cloud>
2023-07-16 21:36:06 +0530

    temp

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-lahiru
+function generateRandomNumber(min, max) {
+    // Generate a random number between min (inclusive) and max (inclusive)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  // Example usage
+  var min = 1;
+  var max = 10;
+  var randomNumber = generateRandomNumber(min, max);
+  console.log("Random number between " + min + " and " + max + ": " + randomNumber);
+  
